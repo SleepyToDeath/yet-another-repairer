b3eb696e99d47b89f82665da78d90d1723672109
Yuepeng Wang <yuepeng.ypwang@gmail.com>
2020-09-17 18:33:55 -0500

    Start to add parser demo

--- src/demo-parser.rkt
+++ src/demo-parser.rkt
@@ -1,37 +1,95 @@
 #lang racket
 
-(require syntax/parse)
+(require (prefix-in demo: syntax/parse))
 (require "grammar.rkt")
 (require "demo-lexer.rkt")
+(require "syntax.rkt")
+(require "syntax-jimple.rkt")
 
 (define (interpret-program program-stx)
-  (syntax-parse program-stx
-    [({~literal program} stmt-stxs ...)
-	 (for ([stmt-stx (syntax->list #'(stmt-stxs ...))])
-	      (interpret-stmt stmt-stx))]))
+  (demo:syntax-parse program-stx
+    [({demo:~literal program} stmt-stxs ...)
+     (begin
+       (define tree
+         (stats (stats-single (stat (stat-nop (nop 0))))))
+       (for ([stmt-stx (syntax->list #'(stmt-stxs ...))])
+            (set! tree (stats (stats-multi tree (stats (stats-single (interpret-stmt stmt-stx)))))))
+       tree)]))
 
 (define (interpret-stmt stmt-stx)
-  (syntax-parse stmt-stx
-    [({~literal statement} ({~literal stmt-ass} stmt-ass-stx ...))
+  (demo:syntax-parse stmt-stx
+    [({demo:~literal statement} ({demo:~literal stmt-ass} stmt-ass-stx ...))
      (interpret-stmt-ass #'(stmt-ass-stx ...))]
-    [({~literal statement} ({~literal stmt-jmp} stmt-jmp-stx ...))
+    [({demo:~literal statement} ({demo:~literal stmt-jmp} stmt-jmp-stx ...))
      (interpret-stmt-jmp #'(stmt-jmp-stx ...))]
-    [({~literal statement} ({~literal stmt-label} stmt-label-stx ...))
+    [({demo:~literal statement} ({demo:~literal stmt-label} stmt-label-stx ...))
      (interpret-stmt-label #'(stmt-label-stx ...))]
-    [({~literal statement} ({~literal stmt-ret} stmt-ret-stx ...))
+    [({demo:~literal statement} ({demo:~literal stmt-ret} stmt-ret-stx ...))
      (interpret-stmt-ret #'(stmt-ret-stx ...))]))
 
 (define (interpret-stmt-ass stmt-ass-stx)
-  (display "ass") (newline))
+  (demo:syntax-parse stmt-ass-stx
+    [(id-stx "=" expr-stx ";")
+     (stat-ass
+       (interpret-id #'id-stx)
+       (interpret-expr #'expr-stx))]))
 
-(define (interpret-stmt-jmp stmt-ass-stx)
-  (display "jmp") (newline))
+(define (interpret-stmt-jmp stmt-jmp-stx)
+  (demo:syntax-parse stmt-jmp-stx
+    [("jmp" expr-stx label-stx ";")
+     (stat-jmp
+       (interpret-expr #'expr-stx)
+       (interpret-label #'label-stx))]))
 
-(define (interpret-stmt-label stmt-ass-stx)
-  (display "label") (newline))
+(define (interpret-stmt-label stmt-label-stx)
+  (demo:syntax-parse stmt-label-stx
+    [("label" label-stx ":")
+     (stat-label
+       (interpret-label #'label-stx))]))
 
-(define (interpret-stmt-ret stmt-ass-stx)
-  (display "ret") (newline))
+(define (interpret-stmt-ret stmt-ret-stx)
+  (stat-ret (nop 0)))
+
+(define (interpret-expr expr-stx)
+  (variable 1000))
+;  (demo:syntax-parse expr-stx
+;    [({demo:~literal j-expr} ({demo:~literal ident} id-stx))
+;     (interpret-id #'id-stx)]
+;    [({demo:~literal j-expr} "(" paren-stx ")")
+;     (interpret-expr-paren #'paren-stx)]
+;    [({demo:~literal j-expr} ({demo:~literal expr} lhs-stx) op-text ({demo:~literal expr} rhs-stx))
+;     (variable 1000)]
+;))
+
+(define (interpret-label label-stx)
+  (demo:syntax-parse label-stx
+    [({demo:~literal ident} l)
+     (label (syntax-e #'l))]))
+
+(define (interpret-id id-stx)
+  (demo:syntax-parse id-stx
+    [({demo:~literal ident} l)
+     (variable (syntax-e #'l))]))
+
+(define (interpret-int int-stx)
+  (const (syntax-e int-stx)))
+
+(define (interpret-expr-bop bop-stx)
+  (demo:syntax-parse bop-stx
+    [(lhs op rhs)
+     (expr-binary
+       (interpret-expr #'lhs)
+       (interpret-op #'op)
+       (interpret-expr #'rhs))]))
+
+(define (interpret-op op-stx)
+  (op (syntax-e op-stx)))
+     
+(define (interpret-expr-paren paren-stx)
+  (demo:syntax-parse paren-stx
+    [("(" expr ")")
+     (interpret-expr #'expr)]))
+
 
 (define program-text
   (string-append
@@ -46,9 +104,14 @@
     "y = v3; \n"))
 
 (define parsed-program
-    (parse (tokenize (open-input-string program-text))))
+  (parse (tokenize (open-input-string program-text))))
 
 ;(syntax->datum parsed-program)
 
-(interpret-program parsed-program)
+(define parsed-ast
+  (interpret-program parsed-program))
+
+;parsed-ast
+
+(ast-print parsed-ast)
 
--- src/grammar.rkt
+++ src/grammar.rkt
@@ -7,13 +7,17 @@ statement ::= stmt-ass
             | stmt-label
             | stmt-ret
 
-stmt-ass   ::= ID "=" expr ";"
-stmt-jmp   ::= "jmp" expr ID ";"
-stmt-label ::= "label" ID ":"
-stmt-ret   ::= "return" expr ";"
+stmt-ass   ::= ident "=" j-expr ";"
+stmt-jmp   ::= "jmp" j-expr ident ";"
+stmt-label ::= "label" ident ":"
+stmt-ret   ::= "return" ";"
 
-expr ::= INTEGER
-       | ID
-       | expr AOP expr
-       | expr BOP expr
-       | "(" expr ")"
+j-expr ::= literal
+       | ident
+       | j-expr BOP j-expr
+       | "(" j-expr ")"
+
+literal ::= INTEGER
+
+ident ::= ID
+
